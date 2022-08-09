6869252035dc0a41aa41782e268840f73c9f3d7f
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-08-09 00:30:37 -0300

    feat: input e botão para a criação de itens da lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import logoImg from './assets/logo.svg';
+import plusIcon from './assets/plus-icon.svg';
 
 import styles from './Header.module.css';
 
 export function App() {
   return (
-    <header className={styles.header}>
-      <img src={logoImg} alt="Logomarca" />
-    </header>
+    <>
+      <header className={styles.header}>
+        <img src={logoImg} alt="Logomarca" />
+      </header>
+
+      <main>
+        <form className={styles.toDoForm}>
+          <div>
+            <div className={styles.toDoLabel}>
+              <label htmlFor='toDoItem'>Tarefa</label>
+            </div>
+            <input className={styles.toDoInput} id='toDoItem' placeholder='Adicione uma nova tarefa' />
+          </div>
+
+          <button className={styles.toDoBtn} type='submit'>
+            Criar
+            <img src={plusIcon} alt="Icone de adição" />
+          </button>
+        </form>
+      </main>
+    </>
   )
 }
