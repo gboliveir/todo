4de92aaf221de5c0c19636f7dd986448c591a462
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-08-10 23:01:30 -0300

    feat: empty element

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logoImg from './assets/logo.svg';
 import plusIcon from './assets/plus-icon.svg';
+import clipboard from './assets/clipboard.svg';
 
 import styles from './Header.module.css';
 
@@ -11,19 +12,38 @@ export function App() {
       </header>
 
       <main>
-        <form className={styles.toDoForm}>
-          <div>
-            <div className={styles.toDoLabel}>
+        <form className={styles.form}>
+          <div className={styles.inputGroup}>
+            <div className={styles.inputLabel}>
               <label htmlFor='toDoItem'>Tarefa</label>
             </div>
-            <input className={styles.toDoInput} id='toDoItem' placeholder='Adicione uma nova tarefa' />
+            <input className={styles.input} id='toDoItem' placeholder='Adicione uma nova tarefa' />
           </div>
 
-          <button className={styles.toDoBtn} type='submit'>
+          <button className={styles.submitButton} type='submit'>
             Criar
             <img src={plusIcon} alt="Icone de adição" />
           </button>
         </form>
+
+        <div className={styles.toDoListContainer}>
+          <header className={styles.toDoListHeader}>
+            <span>
+              Tarefas criadas
+              <div className={styles.toDoCount}>0</div>
+            </span>
+            <span>
+              Concluidas
+              <span className={styles.toDoCount}>0</span>
+            </span>
+          </header>
+
+          <div className={styles.empty}>
+            <img src={clipboard} alt="Imagem de caderneta" />
+            <p className={styles.emptyTopParagraph}>Você não tem tarefas cadastradas</p>
+            <p className={styles.emptyBottomParagraph}>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
       </main>
     </>
   )
